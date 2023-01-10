ebeaca48e24fcd5b1ae9459ce33a55286b3834f2
Tommyval <oyintomiwao@gmail.com>
2023-01-10 21:01:49 +0000

    first

--- lib/Barchart/barchart_occupation.dart
+++ lib/Barchart/barchart_occupation.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter_new/flutter.dart' as charts;
-import 'package:widows_challenge/Models/barchart_occu_model.dart';
+import 'package:widows_challenge/network/network_helper.dart';
 
-class Barchat extends StatelessWidget {
+import '../Models/widow_model.dart';
+
+class Barchat extends StatefulWidget {
   const Barchat({Key? key}) : super(key: key);
 
-  static List<charts.Series<BaroccuModel, String>> _createSampleData() {
-    final data = [
-      BaroccuModel(occupation: 'professional', value: 4),
-      BaroccuModel(occupation: 'Cleric', value: 2),
-      BaroccuModel(occupation: 'Sales', value: 3),
-      BaroccuModel(occupation: 'Skilled Manual', value: 4),
-      BaroccuModel(occupation: 'Skilled Unmanual', value: 3),
-      BaroccuModel(occupation: 'Agriculture', value: 3),
-    ];
+  @override
+  State<Barchat> createState() => _BarchatState();
+}
+
+class _BarchatState extends State<Barchat> {
+  List<ChartModel> chat = [];
+  bool loading = true;
+  final NetworkHelper _networkHelper = NetworkHelper();
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var response = await _networkHelper.get(
+        'https://dl.dropbox.com/s/i8za3yyicvp5qhe/Forged%20Widows%20Data.json?dl=0');
+    List<ChartModel> tempdata = chartModelFromJson(response.body);
+    setState(() {
+      chat = tempdata;
+      loading = false;
+    });
+  }
+
+  List<charts.Series<ChartModel, String>> _createSampleData() {
     return [
-      charts.Series<BaroccuModel, String>(
-        data: data,
-        id: 'occupation',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (BaroccuModel baroccumodel, _) => baroccumodel.occupation,
-        measureFn: (BaroccuModel baroccumodel, _) => baroccumodel.value,
-      )
+      charts.Series<ChartModel, String>(
+          data: chat,
+          id: 'occupation',
+          colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+          domainFn: (ChartModel chatmodel, _) => chatmodel.lga!,
+          measureFn: (ChartModel chatmodel, _) => chatmodel.numberOfChildren)
     ];
   }
 
@@ -29,13 +47,15 @@ class Barchat extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          height: 300,
-          child: charts.BarChart(
-            _createSampleData(),
-            animate: false,
-          ),
-        ),
+        child: loading
+            ? const CircularProgressIndicator()
+            : SizedBox(
+                height: 300,
+                child: charts.BarChart(
+                  _createSampleData(),
+                  animate: false,
+                ),
+              ),
       ),
     );
   }
