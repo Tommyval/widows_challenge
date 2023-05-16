9cd717e5f6cef917d8a46e3971938cceda332e10
Tommyval <oyintomiwao@gmail.com>
2023-05-16 18:54:15 +0100

    edit1

--- lib/ui/screens/widows_data/widows_data_model.dart
+++ lib/ui/screens/widows_data/widows_data_model.dart
@@ -1,13 +1,24 @@
 import 'package:stacked/stacked.dart';
 
 class WidowsDataModel extends BaseViewModel {
+
+  final int _numberOfPages = 10;
+  int _currentPage = 0;
+
+  int get numberOfPages => _numberOfPages;
+  int get currentPage => _currentPage;
+  void changePageIndex(int index) {
+    _currentPage = index;
+    notifyListeners();
+  }
+  
   final List<Map<String, String>> data = [
     {
       'name': 'John Doe',
       'date': '2023-05-01',
       'address': '123 Main St',
       'image':
-          'https://img.freepik.com/free-photo/man-isolated-showing-emotions-end-gestures_1303-30095.jpg',
+           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIkj9z_td8x55pXbGvwXhHfd6d5U7oDpqNktU5zw1kSQ&s',
       'phoneNumber': '+234-233-222',
       'state': 'Ondo'
     },
@@ -16,7 +27,7 @@ class WidowsDataModel extends BaseViewModel {
       'date': '2023-05-05',
       'address': '456 Elm St',
       'image':
-          'https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo=',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIkj9z_td8x55pXbGvwXhHfd6d5U7oDpqNktU5zw1kSQ&s',
       'phoneNumber': '+234-233-222',
       'state': 'Ondo'
     },
@@ -25,7 +36,7 @@ class WidowsDataModel extends BaseViewModel {
       'date': '2023-05-10',
       'address': '789 Oak St',
       'image':
-          'https://media.istockphoto.com/id/1319763895/photo/smiling-mixed-race-mature-man-on-grey-background.jpg?s=612x612&w=0&k=20&c=ZiuzNX9LhTMMcRFrYNfq_zFR7O_aH-q7x1L5elko5uU=',
+           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIkj9z_td8x55pXbGvwXhHfd6d5U7oDpqNktU5zw1kSQ&s',
       'phoneNumber': '+234-233-222',
       'state': 'Ondo'
     },
@@ -34,7 +45,7 @@ class WidowsDataModel extends BaseViewModel {
       'date': '2023-05-10',
       'address': '789 Oak St',
       'image':
-          'https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIkj9z_td8x55pXbGvwXhHfd6d5U7oDpqNktU5zw1kSQ&s',
       'phoneNumber': '+234-233-222',
       'state': 'Ondo'
     },
@@ -43,7 +54,7 @@ class WidowsDataModel extends BaseViewModel {
       'date': '2023-05-10',
       'address': '789 Oak St',
       'image':
-          'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIkj9z_td8x55pXbGvwXhHfd6d5U7oDpqNktU5zw1kSQ&s',
       'phoneNumber': '+234-233-222',
       'state': 'Ondo'
     },
--- lib/ui/screens/widows_data/widows_data_view.dart
+++ lib/ui/screens/widows_data/widows_data_view.dart
@@ -32,116 +32,123 @@ class WidowsDataView extends StatelessWidget {
                     itemCount: viewModel.data.length,
                     itemBuilder: (context, index) {
                       return Card(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.white,
-                                blurRadius: 7.0,
-                              ),
-                            ],
-                            borderRadius: BorderRadius.circular(7.0),
-                          ),
-                          child: Column(
-                            children: [
-                              Flexible(
-                                child: ClipRRect(
-                                  borderRadius: const BorderRadius.only(
-                                      topLeft: Radius.circular(16.0),
-                                      topRight: Radius.circular(16)),
-                                  child: Image.network(
-                                    '${viewModel.data[index]['image']}',
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.white,
+                                  blurRadius: 7.0,
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(7.0),
+                            ),
+                            child: Column(
+                              children: [
+                                Flexible(
+                                  child: ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(16.0),
+                                        topRight: Radius.circular(16)),
+                                    child: Image.network(
+                                      '${viewModel.data[index]['image']}',
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'Name',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    '${viewModel.data[index]['name']}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'Date',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    '${viewModel.data[index]['date']}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'Address',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    '${viewModel.data[index]['address']}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Flexible(
-                                    child: Text(
-                                      'Phone number',
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'Name',
                                       style: TextStyle(
                                           fontWeight: FontWeight.bold),
                                     ),
-                                  ),
-                                  const Spacer(),
-                                  Flexible(
-                                    child: Text(
-                                      '${viewModel.data[index]['phoneNumber']}',
+                                    const Spacer(),
+                                    Text(
+                                      '${viewModel.data[index]['name']}',
                                       style: const TextStyle(fontSize: 16),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'State',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    '${viewModel.data[index]['state']}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  TextButton(
-                                      onPressed: () {},
-                                      child: const Text(
-                                        'view details',
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'Date',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      '${viewModel.data[index]['date']}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'Address',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      '${viewModel.data[index]['address']}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Flexible(
+                                      child: Text(
+                                        'Phone number',
                                         style: TextStyle(
-                                            color: Color(0xff602BF8),
-                                            fontWeight: FontWeight.w400),
-                                      )),
-                                     // Icon(Icons.)
-                                ],
-                              ),
-                            ],
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Flexible(
+                                      child: Text(
+                                        overflow: TextOverflow.ellipsis,
+                                        '${viewModel.data[index]['phoneNumber']}',
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'State',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      '${viewModel.data[index]['state']}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    TextButton(
+                                        onPressed: () {},
+                                        child: const Text(
+                                          'view details',
+                                          style: TextStyle(
+                                              color: Color(0xff602BF8),
+                                              fontWeight: FontWeight.w400),
+                                        )),
+                                    const Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xff602BF8),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
